a2d577bd0f90322d031003fad95e39ff8af91094
rifal-prasetyo <hrdsdms765@gmail.com>
2025-05-28 11:30:53 +0700

    feat: CreateThreadTest entity

---
+++ src/Domains/threads/entities/_test/CreateThread.test.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+const CreateThread = require('../CreateThread');
+
+describe('CreateThread entities', () => {
+  it('should throw error when payload not contain needed property', () => {
+    // Arrange
+    const payload = {
+      title: 'mengapa kenapa',
+    };
+
+    // Action & Assert
+    expect(() => new CreateThread(payload)).toThrow('CREATE_THREAD_NOT_MEET_DATA_TYPE_SPESIFICATION');
+  });
+
+  it('should throw error when payload not contain needed property', () => {
+    // Arrange
+    const payload = {
+      title: true,
+      body: null,
+    };
+
+    // Action & Assert
+    expect(() => new CreateThread(payload)).toThrow('CREATE_THREAD_NOT_MEET_DATA_TYPE_SPESIFICATION');
+  });
+
+  it('should create thread entities correctly', () => {
+    // Arrange
+    const payload = {
+      title: 'AI sangat mengerikan, bagaimana cara bertahan hidup',
+      body: 'Sekarang hampir semua aspek menggunakan kecerdasan buatan, sangat menegerikan',
+    };
+
+    // Action
+    const createThread = new CreateThread(payload);
+    // Assert
+    expect(createThread).toBeInstanceOf(CreateThread);
+    expect(createThread.title).toEqual(payload.title);
+    expect(createThread.body).toEqual(payload.body);
+  });
+});
