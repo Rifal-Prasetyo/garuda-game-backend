3638fb5bf03987c708ff9334c1055e005597227e
rifal-prasetyo <hrdsdms765@gmail.com>
2025-06-01 06:02:52 +0700

    feat: CommentRepositoryInterface

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,19 @@
+class CommentRepository {
+  async addCommentToThread() {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteCommentToThread() {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async addReplyComment() {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteReplyComment() {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
