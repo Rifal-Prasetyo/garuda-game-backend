50649d61ae3d4ebdb8868ecd49b41c3ef073c109
rifal-prasetyo <hrdsdms765@gmail.com>
2025-06-01 06:01:04 +0700

    fix: remove authenticationManager dependency and behavior

--- src/Applications/use_case/CreateThreadUseCase.js
+++ src/Applications/use_case/CreateThreadUseCase.js
@@ -1,16 +1,15 @@
 const CreateThread = require('../../Domains/threads/entities/CreateThread');
 
 class CreateThreadUseCase {
-  constructor({ threadRepository, authenticationManager }) {
+  constructor({ threadRepository }) {
     this._threadRepository = threadRepository;
-    this._authenticationManager = authenticationManager;
   }
 
   async execute(useCasePayload) {
     this._validatePayload(useCasePayload);
-    const { accessToken } = useCasePayload;
+
     const { title, body } = useCasePayload.data;
-    const { id } = await this._authenticationManager.decodePayload(accessToken);
+    const { credentialId: id } = useCasePayload;
     const createThread = new CreateThread({ title, body });
     createThread.owner = id;
     const createdThread = await this._threadRepository.createThread(createThread);
@@ -18,19 +17,15 @@ class CreateThreadUseCase {
   }
 
   _validatePayload(payload) {
-    const { accessToken } = payload;
-    if (!accessToken) {
-      throw new Error('CREATE_THREAD_USE_CASE.NOT_CONTAIN_ACCESS_TOKEN');
-    }
-    if (typeof accessToken !== 'string') {
-      throw new Error('CREATE_THREAD_USE_CASE.ACCESS_TOKEN_NOT_MEET_DATA_TYPE_SPESIFICATION');
+    if (!payload.data) {
+      throw new Error('CREATE_THREAD_USE_CASE.NOT_CONTAIN_NEEDED_PROPERTY');
     }
     const { title, body } = payload.data;
     if (!title || !body) {
-      throw new Error('CREATE_THREAD_USE_CASE.NOT_CONTAIN_DATA');
+      throw new Error('CREATE_THREAD_USE_CASE.NOT_CONTAIN_NEEDED_PROPERTY');
     }
     if (typeof title !== 'string' || typeof body !== 'string') {
-      throw new Error('CREATE_THREAD_USE_CASE.DATA_NOT_MEET_DATA_SPESIFICATION');
+      throw new Error('CREATE_THREAD_USE_CASE.NOT_MEET_DATA_SPESIFICATION');
     }
   }
 }
--- src/Applications/use_case/_test/CreateThreadUseCase.test.js
+++ src/Applications/use_case/_test/CreateThreadUseCase.test.js
@@ -1,41 +1,20 @@
 const CreateThreadUseCase = require('../CreateThreadUseCase');
 const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
-const AuthenticationTokenManager = require('../../security/AuthenticationTokenManager');
 const CreatedThread = require('../../../Domains/threads/entities/CreatedThread');
 
 describe('CreateThreadUseCase', () => {
-  it('should throw error if use case payload not contain access token', async () => {
+  it('should throw error if use case payload not contain needed property', async () => {
     // Arrange
     const useCasePayload = {};
     const createThreadUseCase = new CreateThreadUseCase({});
 
     // Action & Assert
-    await expect(createThreadUseCase.execute(useCasePayload)).rejects.toThrow('CREATE_THREAD_USE_CASE.NOT_CONTAIN_ACCESS_TOKEN');
-  });
-  it('should throw error if access token not string', async () => {
-    // Arrange
-    const useCasePayload = {
-      accessToken: true,
-    };
-    const createThreadUseCase = new CreateThreadUseCase({});
-    // Action & Assert
-    await expect(createThreadUseCase.execute(useCasePayload)).rejects.toThrow('CREATE_THREAD_USE_CASE.ACCESS_TOKEN_NOT_MEET_DATA_TYPE_SPESIFICATION');
-  });
-  it('should throw error if data not contain data', async () => {
-    // Arrange
-    const useCasePayload = {
-      accessToken: 'access_token',
-      data: {},
-    };
-    const createThreadUseCase = new CreateThreadUseCase({});
-
-    // Action & Assert
-    await expect(createThreadUseCase.execute(useCasePayload)).rejects.toThrow('CREATE_THREAD_USE_CASE.NOT_CONTAIN_DATA');
+    await expect(createThreadUseCase.execute(useCasePayload)).rejects.toThrow('CREATE_THREAD_USE_CASE.NOT_CONTAIN_NEEDED_PROPERTY');
   });
   it('should throw error if data not meet data spesification', async () => {
     // Arrange
     const useCasePayload = {
-      accessToken: 'access_token',
+      credentialId: 'user-122',
       data: {
         title: true,
         body: ['title'],
@@ -44,12 +23,12 @@ describe('CreateThreadUseCase', () => {
     const createThreadUseCase = new CreateThreadUseCase({});
 
     // Action & Assert
-    await expect(createThreadUseCase.execute(useCasePayload)).rejects.toThrow('CREATE_THREAD_USE_CASE.DATA_NOT_MEET_DATA_SPESIFICATION');
+    await expect(createThreadUseCase.execute(useCasePayload)).rejects.toThrow('CREATE_THREAD_USE_CASE.NOT_MEET_DATA_SPESIFICATION');
   });
   it('should orchestrating the create thread action correctly', async () => {
     // Arrange
     const useCasePayload = {
-      accessToken: 'accessToken',
+      credentialId: 'user-122',
       data: {
         title: 'Menjadi pria sigma',
         body: 'bagaimana cara menjadi pria sigma fanum tax level 99',
@@ -57,17 +36,14 @@ describe('CreateThreadUseCase', () => {
     };
     //  creating dependency of use case
     const mockThreadRepository = new ThreadRepository();
-    const mockAuthenticationTokenManager = new AuthenticationTokenManager();
     const mockRegisteredUser = new CreatedThread({ id: 'thread-blabalbalbak', title: useCasePayload.data.title, owner: 'user-122' });
 
     // mocking needed function
-    mockAuthenticationTokenManager.decodePayload = jest.fn().mockImplementation(() => Promise.resolve({ username: 'rifal', id: 'user-122' }));
     mockThreadRepository.createThread = jest.fn()
       .mockImplementation(() => Promise.resolve(mockRegisteredUser));
     // create use case instance
     const createThreadUseCase = new CreateThreadUseCase({
       threadRepository: mockThreadRepository,
-      authenticationManager: mockAuthenticationTokenManager,
     });
 
     // Action
@@ -78,8 +54,6 @@ describe('CreateThreadUseCase', () => {
       title: useCasePayload.data.title,
       owner: 'user-122',
     }));
-    expect(mockAuthenticationTokenManager.decodePayload)
-      .toHaveBeenCalledWith(useCasePayload.accessToken);
     expect(mockThreadRepository.createThread).toHaveBeenCalledWith({ ...useCasePayload.data, owner: 'user-122' });
   });
 });
