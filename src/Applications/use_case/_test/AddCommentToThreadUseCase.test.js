331dbeee6474227a803bef313b6cbd3a611dc771
rifal-prasetyo <hrdsdms765@gmail.com>
2025-06-02 06:03:06 +0700

    wip: AddCommentToThreadUseCase

---
+++ src/Applications/use_case/_test/AddCommentToThreadUseCase.test.js
@@ -0,0 +1,62 @@
+const AddCommentToThreadUseCase = require('../AddCommentToThreadUseCase');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const AddedCommentToThread = require('../../../Domains/comments/entities/AddedCommentToThread');
+
+describe('AddCommentToThreadUseCase', () => {
+  it('should throw error when not contain needed property', async () => {
+    // Arrange
+    const useCasePayload = {};
+    const addCommentToThreadUseCase = new AddCommentToThreadUseCase({});
+
+    // Action & Assert
+    await expect(addCommentToThreadUseCase.execute(useCasePayload)).rejects.toThrow('ADD_COMMENT_TO_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+  it('should throw error when not meet data spesification', async () => {
+    // Arrange
+    const useCasePayload = {
+      credentialId: [],
+      threadId: { key: 'value' },
+      data: {
+        content: true,
+      },
+    };
+    const addCommentToThreadUseCase = new AddCommentToThreadUseCase({});
+
+    // Action & Assert
+    await expect(addCommentToThreadUseCase.execute(useCasePayload)).rejects.toThrow('ADD_COMMENT_TO_THREAD.NOT_MEET_DATA_SPESIFICATION');
+  });
+  it('should orchestrating the add comment to thread action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      credentialId: [],
+      threadId: { key: 'value' },
+      data: {
+        content: true,
+      },
+    };
+
+    // creating dependency of use case
+    const mockCommentRepository = new CommentRepository();
+    const mockAddedCommentToThread = new AddedCommentToThread({ id: 'comment-fisjfskf', content: useCasePayload.data.content, owner: 'user-122' });
+
+    // mocking needed function
+    mockCommentRepository.addCommentToThread = jest.fn()
+      .mockImplementation(() => Promise.resolve(mockAddedCommentToThread));
+
+    // create use case instancce
+    const addCommentToThreadUseCase = new AddCommentToThreadUseCase({
+      commentRepository: mockCommentRepository,
+    });
+
+    // Action
+    const actualCommentToThreadUseCase = await addCommentToThreadUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(actualCommentToThreadUseCase).toStrictEqual(new AddedCommentToThread({
+      id: 'comment-fisjfskf',
+      content: useCasePayload.data.content,
+      owner: 'user-122',
+    }));
+    expect(mockCommentRepository.addCommentToThread).toHaveBeenCalledWith({ ...useCasePayload.data, owner: 'user-122' });
+  });
+});
