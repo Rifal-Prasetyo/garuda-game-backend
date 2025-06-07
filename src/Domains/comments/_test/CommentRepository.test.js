76d0c78365ea545cdf92c494cbd7a741822aa467
rifal-prasetyo <hrdsdms765@gmail.com>
2025-06-07 20:46:07 +0700

    feat(comment): CommentRepository fix merge

--- src/Domains/comments/_test/CommentRepository.test.js
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -1,6 +1,5 @@
 const CommentRepository = require('../CommentRepository');
 
-<<<<<<< Updated upstream
 it('should throw error when invoke abstract behavior', async () => {
   // Arrange
   const commentRepository = new CommentRepository();
@@ -10,17 +9,4 @@ it('should throw error when invoke abstract behavior', async () => {
   await expect(commentRepository.deleteCommentToThread({})).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   await expect(commentRepository.addReplyComment({})).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   await expect(commentRepository.deleteReplyComment({})).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-=======
-describe('CommentRepository interface', () => {
-  it('should throw error when invoke unimplemented method', async () => {
-    // Arrange
-    const commentRepository = new CommentRepository();
-
-    // Action & Assert
-    await expect(commentRepository.addCommentToThread('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(commentRepository.deleteCommentToThread('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(commentRepository.addReplyComment('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    await expect(commentRepository.deleteReplyComment('')).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-  });
->>>>>>> Stashed changes
 });
