efac6301714d149bdab58436e345d0d0843659b0
rifal-prasetyo <hrdsdms765@gmail.com>
2025-06-07 20:43:08 +0700

    feat(comment): CommentRepository

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -1,5 +1,6 @@
 class CommentRepository {
   async addCommentToThread() {
+<<<<<<< Updated upstream
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
@@ -13,6 +14,21 @@ class CommentRepository {
 
   async deleteReplyComment() {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+=======
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteCommentToThread() {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async addReplyComment() {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteReplyComment() {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+>>>>>>> Stashed changes
   }
 }
 
