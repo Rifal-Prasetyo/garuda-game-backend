946ccff1b341c2950fdd72976ce59075080da97e
rifal-prasetyo <hrdsdms765@gmail.com>
2025-07-05 20:20:15 +0700

    rev -7

--- src/Domains/threads/entities/_test/DetailThread.test.js
+++ src/Domains/threads/entities/_test/DetailThread.test.js
@@ -48,10 +48,17 @@ describe('DetailThread entity', () => {
           is_delete: false,
         },
         {
+          id: 'comment-2',
+          username: 'user2',
+          date: new Date('2022-01-01T01:00:00.000Z'),
+          content: 'Komentar kedua',
+          is_delete: false,
+        },
+        {
           id: 'reply-1',
           username: 'user3',
           date: new Date('2022-01-01T01:10:00.000Z'),
-          content: 'Balasan',
+          content: 'Balasan komentar 1',
           commentId: 'comment-1',
           is_delete: false,
         },
@@ -72,24 +79,34 @@ describe('DetailThread entity', () => {
     expect(detailThread.username).toBe(payload.username);
     expect(typeof detailThread.username).toBe('string');
     expect(Array.isArray(detailThread.comments)).toBe(true);
-    expect(detailThread.comments.length).toBe(1);
-    const comment = detailThread.comments[0];
-    expect(comment.id).toBe('comment-1');
-    expect(typeof comment.id).toBe('string');
-    expect(comment.username).toBe('user2');
-    expect(typeof comment.username).toBe('string');
-    expect(comment.date).toBeInstanceOf(Date);
-    expect(comment.content).toBe('Komentar pertama');
-    expect(typeof comment.content).toBe('string');
-    expect(Array.isArray(comment.replies)).toBe(true);
-    expect(comment.replies.length).toBe(1);
-    const reply = comment.replies[0];
+    expect(detailThread.comments.length).toBe(2);
+    const comment1 = detailThread.comments[0];
+    expect(comment1.id).toBe('comment-1');
+    expect(typeof comment1.id).toBe('string');
+    expect(comment1.username).toBe('user2');
+    expect(typeof comment1.username).toBe('string');
+    expect(comment1.date).toBeInstanceOf(Date);
+    expect(comment1.content).toBe('Komentar pertama');
+    expect(typeof comment1.content).toBe('string');
+    expect(Array.isArray(comment1.replies)).toBe(true);
+    expect(comment1.replies.length).toBe(1);
+    const comment2 = detailThread.comments[1];
+    expect(comment2.id).toBe('comment-2');
+    expect(typeof comment2.id).toBe('string');
+    expect(comment2.username).toBe('user2');
+    expect(typeof comment2.username).toBe('string');
+    expect(comment2.date).toBeInstanceOf(Date);
+    expect(comment2.content).toBe('Komentar kedua');
+    expect(typeof comment2.content).toBe('string');
+    expect(Array.isArray(comment2.replies)).toBe(true);
+    expect(comment2.replies.length).toEqual(0);
+    const reply = comment1.replies[0];
     expect(reply.id).toBe('reply-1');
     expect(typeof reply.id).toBe('string');
     expect(reply.username).toBe('user3');
     expect(typeof reply.username).toBe('string');
     expect(reply.date).toBeInstanceOf(Date);
-    expect(reply.content).toBe('Balasan');
+    expect(reply.content).toBe('Balasan komentar 1');
     expect(typeof reply.content).toBe('string');
     expect(reply.commentId).toBeUndefined();
     expect(reply.is_delete).toBeUndefined();
